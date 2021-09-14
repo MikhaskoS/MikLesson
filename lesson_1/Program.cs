022c594cdc991779ff4cc05a9aa75c6f611febd3
Сергей M <mikhassergej@yandex.ru>
2021-09-14 18:57:31 +0300

    Циклы

--- lesson_1/Program.cs
+++ lesson_1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lesson_1
 {
@@ -10,52 +6,28 @@ namespace lesson_1
     {
         static void Main(string[] args)
         {
-            //int k = 1;
-            //int a1 = 0;
-            //int a2 = 1;
 
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    if (i < 2)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //    else
-            //    {
-            //        //int a = a1 + a2;
-            //        //Console.WriteLine(a);
-            //        //a1 = a2;
-            //        //a2 = a;
+            int k = 10;
+            double d = 1.05224;
+            string s = "Вася";
 
-            //    }
-            //}
-            //Console.WriteLine("\n------------------");
+            //Console.WriteLine($"double.MaxValue = {double.MaxValue}");
+            //Console.WriteLine($"int16.MaxValue = {Int16.MaxValue}");
+            //Console.WriteLine($"int32.MaxValue = {Int32.MaxValue}");
 
+            string a1 = "привет";
+            string a2 = "вася!";
+            char r = '=';
 
-            //double a = 6;
-            //double b = 4;
-            //Console.WriteLine(a % b);
+            Console.WriteLine(a1 +a2);
 
-            //int t = 4;
-            //int f = t++;
-            //int r = ++t;
+            Сycles.Printer();
+            
 
-            //Console.WriteLine($"t: {t++}");
-            //Console.WriteLine($"f: {f}");
-            //Console.WriteLine($"r: {r}");
-
-            int k = 0;
-            for (int i = 0; i < 1_000_000; i++) 
-            {
-                if (i % 2 == 0 && i % 3 == 0)
-                {
-                    k = k + 1;   //Console.WriteLine(i);
-                }
-            }
-
-            Console.WriteLine(k);
             Console.ReadLine();
         }
 
+
+       
     }
 }
---
+++ lesson_1/Сycles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace lesson_1
+{
+    class Сycles
+    {
+        /// <summary> Описание функции. Ля-ля. </summary>
+        public static void Printer()
+        {
+            Console.WriteLine("Функция Printer!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i} ");
+            }
+
+
+            int k = 0;
+            while (k < 100)
+            {
+                k++;
+                Console.Write($"{k} ");
+            }
+
+            Console.WriteLine();
+
+            int n = 0;
+            do
+            {
+                n++;
+                Console.Write($"{n} ");
+               
+            } while (n < 100);
+
+            Console.WriteLine();
+
+            string str = "Мища!";
+
+            foreach (char ch in str)
+                Console.WriteLine(ch);
+        }
+    }
+}
