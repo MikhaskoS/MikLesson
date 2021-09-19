833482b96f4c52cfc1946a4d558089f05e04302d
Сергей M <mikhassergej@yandex.ru>
2021-09-19 17:53:47 +0300

    DictionaryTest - по немецкому

---
+++ DictionaryTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DictionaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // массивы
+            string[] rus = new[] { "грустный", "спокойный","активный","романтичный","дикий","уставший","веселый","злой"};
+            string[] deutsch = new[] { "traurig", "ruhig","aktiv","romantisch","wild", "mude","froh", "bose" };
+            Word[] words = new Word[rus.Length];
+
+            // заполняем массив
+            for (int i = 0; i < words.Length; i++)
+            {
+                Word word = new Word()
+                {
+                    TextRus = rus[i],
+                    TextTranslation = deutsch[i]
+                };
+
+                words[i] = word;
+            }
+
+
+            //foreach (Word w in words)
+            //    Console.WriteLine(w.TextTranslation);
+
+            bool res = true;
+            int q = 0;
+            while(res)
+            {
+                if (words[q].attempt >= 0)
+                {
+                    Console.WriteLine($"Перевод слова {words[q].TextRus} : ");
+                    string translate = Console.ReadLine();
+
+                    if (translate == words[q].TextTranslation)
+                    {
+                        Console.WriteLine("Верно!");
+                        words[q].attempt = words[q].attempt - 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверно!");
+                        words[q].attempt = words[q].attempt + 1;
+                    }
+                }
+
+                q++;
+                if (q > words.Length - 1) q = 0;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DictionaryTest/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTest
+{
+    public class Word
+    {
+        public string TextRus;
+        public string TextTranslation;
+        public int attempt;
+
+        public Word()
+        {
+            attempt = 0;
+        }
+    }
+}
