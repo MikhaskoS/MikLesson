99f99323bf559c5a099403165180d5bc92e1e9fe
Сергей M <mikhassergej@yandex.ru>
2021-10-16 16:35:57 +0300

    Игра - Виселица

---
+++ Gallows/MyFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+    public class MyFunction
+    {
+        public static bool CharInString(string str, char ch)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ch) return true;
+
+            }
+            return false;
+        }
+
+        public static string Replace(string word_original, string word_finding, char ch)
+        {
+            char[] result = word_finding.ToArray();
+
+            for (int i = 0; i < word_original.Length; i++)
+            {
+                if (word_original[i] == ch)
+                {
+                    result[i] = ch;
+                }
+            }
+
+            return new string(result);
+        }
+    }
+}
---
+++ Gallows/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gallows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word_original = "машина";
+            byte attempt = 5;
+            string word = new string('*', word_original.Length);
+
+            Console.WriteLine($"Дарова, отгадай моё слово из {word_original.Length} букв");
+
+            while(attempt > 0)
+            {
+                Console.WriteLine($"Угадай букву в моём слове {word}:");
+               
+                ConsoleKeyInfo a = Console.ReadKey();
+                char ch = a.KeyChar;
+                Console.WriteLine();
+
+                bool yes = MyFunction.CharInString(word_original, ch);
+                if (yes)
+                {
+                    Console.WriteLine("да");
+                    word = MyFunction.Replace(word_original, word, ch);
+                }
+                else
+                {
+                    Console.WriteLine("нет");
+                    attempt--;
+                }
+            }
+        }
+    }
+}
