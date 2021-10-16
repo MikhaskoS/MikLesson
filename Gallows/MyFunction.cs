96dda56c721d74c6b8befee5afbc263891fb0a76
Сергей M <mikhassergej@yandex.ru>
2021-10-16 18:59:20 +0300

    Игра в Виселицу

--- Gallows/MyFunction.cs
+++ Gallows/MyFunction.cs
@@ -32,5 +32,102 @@ namespace Gallows
 
             return new string(result);
         }
+
+        public static bool IsGameOver(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == '*') return false;
+
+
+            }
+            return true;
+        }
+
+        public static void Skeleton(int attept)
+        {
+            string s1=
+@"--------------
+      
+      
+      
+     
+    
+      
+     / \
+    |   | 
+==============
+  ||     ||
+     ";
+            string s2 =
+@"--------------
+      
+      
+      
+     
+      |
+      |  
+     / \
+    |   | 
+==============
+  ||     ||
+     ";
+            string s3 =
+@"--------------
+      
+      
+      
+     
+    \-|-/
+      |  
+     / \
+    |   | 
+==============
+  ||     ||
+     ";
+            string s4 =
+@"--------------
+      
+      
+      
+     ( )
+    \-|-/
+      |  
+     / \
+    |   | 
+==============
+  ||     ||
+     ";
+            string s5 =
+@"--------------
+      |
+      |
+      |
+     ( )
+    \-|-/
+      |
+     / \
+    |   |   
+     ";
+            switch (attept)
+            {
+                case 1:
+                    Console.WriteLine(s1);
+                    break;
+                case 2:
+                    Console.WriteLine(s2);
+                    break;
+                case 3:
+                    Console.WriteLine(s3);
+                    break;
+                case 4:
+                    Console.WriteLine(s4);
+                    break;
+                case 5:
+                    Console.WriteLine(s5);
+                    break;
+            }
+        }
+
     }
 }
--- Gallows/Program.cs
+++ Gallows/Program.cs
@@ -6,18 +6,28 @@ namespace Gallows
     {
         static void Main(string[] args)
         {
-            string word_original = "машина";
-            byte attempt = 5;
+            string word_original = "превысокомногорассмотрительствующий";
+            word_original = word_original.ToUpper();
+            
+            byte attempt = 0;
             string word = new string('*', word_original.Length);
 
-            Console.WriteLine($"Дарова, отгадай моё слово из {word_original.Length} букв");
+            Console.WriteLine($"Дарова, отгадай моё слово из {word_original.Length} букв.");
+            Console.WriteLine("Готов?");
+            if (Console.ReadKey(true).Key != ConsoleKey.Y)
+            { 
+                attempt = 10; 
+            }
 
-            while(attempt > 0)
+            while(attempt < 5)
             {
+                Console.Clear();
                 Console.WriteLine($"Угадай букву в моём слове {word}:");
                
-                ConsoleKeyInfo a = Console.ReadKey();
+                ConsoleKeyInfo a = Console.ReadKey(true);
                 char ch = a.KeyChar;
+                ch = Char.ToUpper(ch);
+                
                 Console.WriteLine();
 
                 bool yes = MyFunction.CharInString(word_original, ch);
@@ -25,12 +35,26 @@ namespace Gallows
                 {
                     Console.WriteLine("да");
                     word = MyFunction.Replace(word_original, word, ch);
+                    bool game_over = MyFunction.IsGameOver(word);
+                    if (game_over)
+                    {
+                        Console.WriteLine("Ти выиграл, уря!");
+                        break;
+                    }
                 }
                 else
                 {
                     Console.WriteLine("нет");
-                    attempt--;
+                    attempt++;
+                    MyFunction.Skeleton(attempt);
+                    if (attempt == 5)
+                    {
+                        Console.WriteLine("Ти проиграл, попуск!");
+                    }
+                    Console.ReadLine();
                 }
+
+               
             }
         }
     }
