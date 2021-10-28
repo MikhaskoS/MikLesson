cf6c558ac408088b989e786acae5ddd9a7e98ab2
Сергей M <mikhassergej@yandex.ru>
2021-10-28 16:59:00 +0300

    Функции от Копатыча

---
+++ lesson_3/MyClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_3
+{
+    class MyClass
+    {
+        public void Swap(ref int a,ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }                                                  
+    
+        /// <summary>
+        /// сумма чисел
+        /// </summary>
+        /// <param name="a">коровы</param>
+        /// <param name="b">овцы</param>
+        /// <returns>кирпичи</returns>
+        public int Sum(int a, int b)
+        {
+            int res = a + b;
+            return res;
+        }
+
+        public string Sum(string a, string b)
+        {
+            string res = a + b;
+            return res;
+        }
+
+        public int Sum(int a, int b, int c)
+        {
+            int res = a + b + c;
+            return res;
+        }
+
+        public static int Factorial(int n)
+        {
+            int k = 0;
+            int res = 1;
+            while(k < n)
+            {
+                k++;
+                res = res * k;
+            }
+            return res;
+        }
+        
+        public static decimal F (decimal n)
+        {
+            if (n == 0) return 1;
+
+            return n * F(n - 1);
+        }
+    
+    }
+
+}
---
+++ lesson_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine($"a = {a} b = {b}"); 
+
+            MyClass m = new MyClass();
+            m.Swap(ref a, ref b);
+            int res = m.Sum(a, b);
+
+            //m.Factorial(4);
+
+
+
+            Console.WriteLine(MyClass.F(29));
+            Console.WriteLine($"a = {a} b = {b}");
+
+            
+
+           
+
+            
+        }
+    }
+}
