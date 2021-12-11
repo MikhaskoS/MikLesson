c076c4ebb740fec190901f183ab3ec640284d9ba
MIKHASKOM <mmikhasko@yandex.ru>
2021-12-11 17:50:38 +0300

    Дроби

---
+++ ArithmeticFraction/Fraction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticFraction
+{
+    public class Fraction
+    {
+        private int _numerator;
+        private int _denomenator;
+
+        public int Numerator { get => _numerator; set => _numerator = value; }
+        public int Denomenator { get => _denomenator; set => _denomenator = value; }
+
+        /// <summary>
+        /// конструктор дроби
+        /// </summary>
+        /// <param name="numerator">числитель</param>
+        /// <param name="denomenator">знаменатель</param>
+        public Fraction(int numerator, int denomenator)
+        {
+            Numerator = numerator;
+            _denomenator = denomenator;
+        }
+
+
+        private int NOD(int a, int b)
+        {
+            if (a < b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            int nod = 0;
+            int r = -1;
+
+            while (r != 0)
+            {
+                int n = a / b;
+                r = a % b;
+
+                if (r == 0)
+                {
+                    nod = b;
+                }
+                else
+                {
+                    a = b;
+                    b = r;
+                }
+            }
+
+            return nod;
+        }
+
+        /// <summary>
+        /// сократить дробь 
+        /// </summary>
+        public void Reduction()
+        {
+            int nod = NOD(Numerator, _denomenator);
+            if (nod == 1) return;
+
+            Numerator = Numerator / nod;
+            _denomenator = _denomenator / nod;
+        }
+
+        public void Flip()
+        {
+            int t = Numerator;
+            Numerator = _denomenator;
+            _denomenator = t;
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine($"{Numerator}/{_denomenator}");
+        }
+        public void PrintCorrect()
+        {
+            if (_denomenator > Numerator) Print();
+            else
+            {
+                int a = Numerator / _denomenator;
+                int n = Numerator % _denomenator;
+                if (n == 0) 
+                    Console.WriteLine($"{a}");
+                else 
+                    Console.WriteLine($"{a}_{n}/{_denomenator}");
+            }
+        }
+
+        static public Fraction Add(Fraction f1, Fraction f2)
+        {
+            int n = f1.Numerator * f2.Denomenator + f1.Denomenator * f2.Numerator;
+            int t = f1.Denomenator * f2.Denomenator;
+
+            Fraction res = new Fraction(n, t);
+            res.Reduction();
+
+            return res;
+        }
+    }
+}
---
+++ ArithmeticFraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArithmeticFraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Fraction d1 = new Fraction(12348, 4568);
+
+            //Console.WriteLine("Hello World!");
+
+            //d1.Print();
+            //d1.Reduction();
+            //d1.Print();
+            //d1.Flip();
+            //d1.Print();
+
+            //Fraction d2 = new Fraction(3568, 254);
+            //d2.Reduction();
+            //d2.PrintCorrect();
+
+            Fraction d1 = new Fraction(1, 3);
+            Fraction d2 = new Fraction(1, 5);
+
+            Fraction res = Fraction.Add(d1, d2);
+            res.Print();
+        }
+    }
+}
